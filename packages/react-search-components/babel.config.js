f826e0c8bfd2a7f678472ba6c9001dceff96f78e
Jason Stoltzfus <jastoltz24@gmail.com>
2018-12-13 16:16:05 -0500

    Added support for exporting ES modules
    Babel now builds transpiles to two different directories:
    
    /lib - This contains transpiled code in CommonJS. It is exported by "main"
    in package.json. It will be used by tools that do not support ES Modules,
    like webpack.
    
    /es - This contains transpiled code in ES Modules. It is exported by "module"
    and will be used by build tools that support ES Modules, like Rollup.
    
    Additionally, I've added the "sideEffects": false flag to package.json,
    which enables tree shaking in webpack.

--- packages/react-search-components/babel.config.js
+++ packages/react-search-components/babel.config.js
@@ -1,4 +1,17 @@
-const presets = ["@babel/env", "@babel/preset-react"];
+const presets = [
+  [
+    "@babel/env",
+    {
+      targets: {
+        browsers: ["last 2 versions", "> 5%"]
+      },
+      modules: process.env.BABEL_MODULES
+        ? process.env.BABEL_MODULES
+        : "commonjs" // babel's default is commonjs
+    }
+  ],
+  "@babel/preset-react"
+];
 
 module.exports = {
   sourceMaps: "inline",
