c2ff749fb271384024c42a638569c035c6423326
Jason Stoltzfus <jastoltz24@gmail.com>
2019-03-12 14:44:37 -0400

    SearchBox to Downshift + autocomplete
    Downshift was added to enable autocomplete. I've added 3 new parameters
    to the SearchBox view:
    
    - autocomplete - Whether or not to show autocompletes
    - autocompleteItems - Accepts array of "Sections", so that we can
    have titles above various types of autocomplete, i.e, suggestions and
    results
    - onSelectAutocomplete - A callback to trigger when a selection
    is made in the autocomplete.

--- packages/react-search-ui-views/src/SearchBox.js
+++ packages/react-search-ui-views/src/SearchBox.js
@@ -1,25 +1,113 @@
 import PropTypes from "prop-types";
 import React from "react";
+import Downshift from "downshift";
 
 function SearchBox(props) {
-  const { isFocused, inputProps, onChange, onSubmit, value } = props;
+  const {
+    autocomplete,
+    autocompleteItems = [],
+    isFocused,
+    inputProps,
+    onChange,
+    onSelectAutocomplete,
+    onSubmit,
+    value
+  } = props;
   const focusedClass = isFocused ? "focus" : "";
 
+  // TODO Remove inline styles
   return (
-    <form className="sui-search-box" onSubmit={onSubmit}>
-      <input
-        className={`sui-search-box__text-input ${focusedClass}`}
-        onChange={onChange}
-        type="text"
-        value={value}
-        placeholder="Search your documents&#8230;"
-        {...inputProps}
-      />
-      <input
-        type="submit"
-        value="Search"
-        className="button sui-search-box__submit"
-      />
+    <form onSubmit={onSubmit}>
+      <Downshift
+        inputValue={value}
+        onChange={onSelectAutocomplete}
+        onInputValueChange={onChange}
+        itemToString={item => (item ? item.value : "")}
+      >
+        {({
+          getInputProps,
+          getItemProps,
+          getMenuProps,
+          isOpen,
+          highlightedIndex,
+          selectedItem
+        }) => {
+          let index = 0;
+          return (
+            <div
+              className="sui-search-box"
+              style={{ position: "relative", overflow: "visible" }}
+            >
+              <input
+                {...getInputProps({
+                  placeholder: "Search your documents",
+                  ...inputProps,
+                  className: `sui-search-box__text-input ${focusedClass}`
+                })}
+              />
+              <ul
+                {...getMenuProps({
+                  style: {
+                    position: "absolute",
+                    top: "100%",
+                    width: "100%"
+                  }
+                })}
+              >
+                {autocomplete && isOpen
+                  ? autocompleteItems.map((section, i) => {
+                      return (
+                        <div key={i}>
+                          {section.title && <div>{section.title}</div>}
+                          <ul>
+                            {section.items.map(item => {
+                              index++;
+                              return (
+                                // eslint-disable-next-line react/jsx-key
+                                <li
+                                  {...getItemProps({
+                                    key: item.id,
+                                    index: index - 1,
+                                    item,
+                                    style: {
+                                      backgroundColor:
+                                        highlightedIndex === index - 1
+                                          ? "lightgray"
+                                          : "white",
+                                      fontWeight:
+                                        selectedItem === item
+                                          ? "bold"
+                                          : "normal"
+                                    }
+                                  })}
+                                >
+                                  {item.snippet ? (
+                                    <span
+                                      dangerouslySetInnerHTML={{
+                                        __html: item.snippet
+                                      }}
+                                    />
+                                  ) : (
+                                    <span>{item.raw}</span>
+                                  )}
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </div>
+                      );
+                    })
+                  : null}
+              </ul>
+              <input
+                type="submit"
+                value="Search"
+                className="button sui-search-box__submit"
+              />
+            </div>
+          );
+        }}
+      </Downshift>
     </form>
   );
 }
@@ -28,6 +116,20 @@ SearchBox.propTypes = {
   onChange: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
   value: PropTypes.string.isRequired,
+  autocomplete: PropTypes.bool,
+  autocompleteItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      items: PropTypes.arrayOf(
+        PropTypes.shape({
+          raw: PropTypes.string,
+          snippet: PropTypes.string,
+          value: PropTypes.any
+        })
+      )
+    })
+  ),
+  onSelectAutocomplete: PropTypes.func,
   inputProps: PropTypes.object,
   isFocused: PropTypes.bool
 };
--- packages/react-search-ui-views/stories/SearchBox.stories.js
+++ packages/react-search-ui-views/stories/SearchBox.stories.js
@@ -10,15 +10,87 @@ const baseProps = {
   onSubmit: e => {
     e.preventDefault();
     action("submitted")();
-  }
+  },
+  value: ""
 };
 
+const autocompleteItems = [
+  {
+    title: "Results",
+    items: [
+      { id: "1", snippet: "<em>Bike</em> Cops", raw: "", value: "Bike Cops" },
+      { id: "2", snippet: "<em>Biker</em> Gang", raw: "", value: "Biker Gang" },
+      { id: "3", snippet: "<em>Biker</em> Bar", raw: "", value: "Biker Bar" },
+      {
+        id: "4",
+        snippet: "Best <em>bikes</em> of 2010",
+        raw: "",
+        value: "Best bikes of 2010"
+      },
+      {
+        id: "5",
+        snippet: "<em>Bike</em> seats and accessories",
+        raw: "",
+        value: "Bike seats and accessories"
+      }
+    ]
+  },
+  {
+    title: "Suggestions",
+    items: [
+      { id: "6", snippet: "", raw: "bike", value: "bike" },
+      { id: "7", snippet: "", raw: "bike police", value: "bike police" },
+      { id: "8", snippet: "", raw: "bike police go", value: "bike police go" },
+      { id: "9", snippet: "", raw: "fast bikes", value: "fast bikes" },
+      { id: "10", snippet: "", raw: "bike race", value: "bike race" }
+    ]
+  }
+];
+
+// This wrapper is here just to store "value" state somewhere, so
+// that when you type in the SearchBox, specifically for autocomplete,
+// something actually changes on the page.
+class Wrapper extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: ""
+    };
+  }
+
+  render() {
+    return (
+      <SearchBox
+        autocomplete={true}
+        autocompleteItems={autocompleteItems}
+        value={this.state.value}
+        onSelectAutocomplete={selection => {
+          action("selectAutocomplete")(selection);
+        }}
+        onSubmit={e => {
+          e.preventDefault();
+          action("submitted")();
+        }}
+        onChange={value => {
+          action("changed")();
+          this.setState({
+            value
+          });
+        }}
+        {...this.props}
+      />
+    );
+  }
+}
+
 storiesOf("SearchBox", module)
-  .add("no value", () => <SearchBox {...{ ...baseProps }} />)
-  .add("with value", () => <SearchBox {...{ value: "value", ...baseProps }} />)
-  .add("with focus", () => <SearchBox {...{ isFocused: true, ...baseProps }} />)
+  .add("no value", () => <SearchBox {...baseProps} />)
+  .add("with value", () => <SearchBox {...baseProps} value="value" />)
+  .add("with focus", () => <SearchBox {...baseProps} isFocused={true} />)
   .add("with inputProps", () => (
     <SearchBox
-      {...{ inputProps: { placeholder: "custom placeholder" }, ...baseProps }}
+      {...baseProps}
+      inputProps={{ placeholder: "custom placeholder" }}
     />
-  ));
+  ))
+  .add("with autocomplete", () => <Wrapper />);
