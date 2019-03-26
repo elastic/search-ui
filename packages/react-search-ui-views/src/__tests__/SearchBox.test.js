3934d4b60614c566b4f9914b19f8b51c382128c4
Jason Stoltzfus <jastoltz24@gmail.com>
2019-03-26 11:10:11 -0400

    Full Autocomplete support for Search Results (#143)
    - Added working example to the Sandbox
    - Closes the autocomplete Autocomplete any time a query is submitted
    - Ensure the autocomplete value is not changed whenever a result is selected
    - Support for boolean configuration of autocompleteResults (explained in design doc)
    - Updated `FieldValue` type to expect Arrays of values from the server.

--- packages/react-search-ui-views/src/__tests__/SearchBox.test.js
+++ packages/react-search-ui-views/src/__tests__/SearchBox.test.js
@@ -5,6 +5,8 @@ import { shallow } from "enzyme";
 const requiredProps = {
   onChange: () => {},
   onSubmit: () => {},
+  autocompletedResults: [],
+  autocompletedSuggestions: {},
   value: "test"
 };
 
