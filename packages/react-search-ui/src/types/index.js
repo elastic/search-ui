c910ff05c055adf882d564972a47ccd8502cea6e
Jason Stoltzfus <jastoltz24@gmail.com>
2019-04-08 14:04:02 -0400

    React Search UI suggestions support

--- packages/react-search-ui/src/types/index.js
+++ packages/react-search-ui/src/types/index.js
@@ -8,5 +8,6 @@ export { default as FilterValue } from "./FilterValue";
 export { default as FilterValueValue } from "./FilterValueValue";
 export { default as FilterValueRange } from "./FilterValueRange";
 export { default as Result } from "./Result";
+export { default as Suggestion } from "./Suggestion";
 
 export { default as SortOption } from "./SortOption";
