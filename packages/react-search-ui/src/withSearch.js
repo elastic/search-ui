35027eb01e63ed6f3bce78fc97ea51f40f216496
Jason Stoltzfus <jastoltz24@gmail.com>
2019-05-07 15:44:34 -0400

    Changed provider to not setState

--- packages/react-search-ui/src/withSearch.js
+++ packages/react-search-ui/src/withSearch.js
@@ -10,13 +10,27 @@ import SearchConsumer from "./SearchConsumer";
  * `mapContextToProps` can be used to manipulate actions and state from context
  * before they are passed on to the container.
  */
+
+function buildContextForProps(context) {
+  return {
+    ...context.driver.getState(),
+    ...context.driver.getActions()
+  };
+}
+
 export default function withSearch(Component) {
   return function WithSearch(props) {
+    // eslint-disable-next-line react/prop-types
     const { mapContextToProps = context => context, ...rest } = props;
 
     return (
       <SearchConsumer>
-        {context => <Component {...mapContextToProps(context)} {...rest} />}
+        {context => (
+          <Component
+            {...mapContextToProps(buildContextForProps(context))}
+            {...rest}
+          />
+        )}
       </SearchConsumer>
     );
   };
