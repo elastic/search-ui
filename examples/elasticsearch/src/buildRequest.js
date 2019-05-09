279ee518af793e75eae6bea584b802bb17096779
Jason Stoltzfus <jastoltz24@gmail.com>
2019-05-09 16:32:16 -0400

    Added support for filters

--- examples/elasticsearch/src/buildRequest.js
+++ examples/elasticsearch/src/buildRequest.js
@@ -1,4 +1,5 @@
 function calculateFrom(current, resultsPerPage) {
+  if (!current || !resultsPerPage) return;
   return (current - 1) * resultsPerPage;
 }
 
@@ -6,6 +7,17 @@ function calculateSort(sortDirection, sortField) {
   if (sortDirection && sortField) {
     return [{ [`${sortField}.keyword`]: sortDirection }];
   }
+}
+
+function calculateQuery(searchTerm) {
+  return searchTerm
+    ? {
+        multi_match: {
+          query: searchTerm,
+          fields: ["title", "description"]
+        }
+      }
+    : { match_all: {} };
 }
 
 export default function buildRequest(state) {
@@ -20,29 +32,65 @@ export default function buildRequest(state) {
   } = state;
 
   const sort = calculateSort(sortDirection, sortField);
-
-  const query = searchTerm
-    ? {
-        multi_match: {
-          query: searchTerm,
-          fields: ["title", "description"]
-        }
-      }
-    : { match_all: {} };
+  const query = calculateQuery(searchTerm);
+  const size = resultsPerPage;
+  const from = calculateFrom(current, resultsPerPage);
 
   const body = {
-    query,
+    // Static query Configuration
+    // --------------------------
+    // https://www.elastic.co/guide/en/elasticsearch/reference/7.x/search-request-highlighting.html
     highlight: {
       fragment_size: 200,
       number_of_fragments: 1,
       fields: {
-        title: {}
+        title: {},
+        description: {}
       }
     },
+    //https://www.elastic.co/guide/en/elasticsearch/reference/7.x/search-request-source-filtering.html#search-request-source-filtering
+    _source: ["id", "nps_link", "title", "description"],
+    aggs: {
+      states: { terms: { field: "states.keyword", size: 30 } },
+      world_heritage_site: {
+        terms: { field: "world_heritage_site" }
+      },
+      visitors: {
+        range: {
+          field: "visitors",
+          ranges: [
+            { from: 0.0, to: 10000.0, key: "0 - 10000" },
+            { from: 10001.0, to: 100000.0, key: "10001 - 100000" },
+            { from: 100001.0, to: 500000.0, key: "100001 - 500000" },
+            { from: 500001.0, to: 1000000.0, key: "500001 - 1000000" },
+            { from: 1000001.0, to: 5000000.0, key: "1000001 - 5000000" },
+            { from: 5000001.0, to: 10000000.0, key: "5000001 - 10000000" },
+            { from: 10000001.0, key: "10000001+" }
+          ]
+        }
+      },
+      acres: {
+        range: {
+          field: "acres",
+          ranges: [
+            { from: -1.0, key: "Any" },
+            { from: 0.0, to: 1000.0, key: "Small" },
+            { from: 1001.0, to: 100000.0, key: "Medium" },
+            { from: 100001.0, key: "Large" }
+          ]
+        }
+      }
+    },
+
+    // Dynamic values based on current Search UI state
+    // --------------------------
+    // https://www.elastic.co/guide/en/elasticsearch/reference/7.x/full-text-queries.html
+    query,
+    // https://www.elastic.co/guide/en/elasticsearch/reference/7.x/search-request-sort.html
     ...(sort && { sort }),
-    ...(resultsPerPage && { size: resultsPerPage }),
-    ...(resultsPerPage &&
-      current && { from: calculateFrom(current, resultsPerPage) })
+    // https://www.elastic.co/guide/en/elasticsearch/reference/7.x/search-request-from-size.html
+    ...(size && { size }),
+    ...(from && { from })
   };
 
   return body;
--- examples/elasticsearch/src/buildState.js
+++ examples/elasticsearch/src/buildState.js
@@ -1,8 +1,3 @@
-const addEachKeyValueToObject = (acc, [key, value]) => ({
-  ...acc,
-  [key]: value
-});
-
 function totalPages(resultsPerPage, totalResults) {
   if (totalResults === 0) return 1;
   return Math.ceil(totalResults / resultsPerPage);
@@ -11,29 +6,124 @@ function totalPages(resultsPerPage, totalResults) {
 function getInfo(hits, resultsPerPage) {
   const totalResults = hits.total.value;
   return {
-    totalPages: resultsPerPage ? totalPages(resultsPerPage, totalResults) : 0, // TODO
+    totalPages: resultsPerPage ? totalPages(resultsPerPage, totalResults) : 0,
     totalResults
   };
 }
 
+function getHighlight(hit, fieldName) {
+  if (hit._source.title === "Rocky Mountain" && fieldName === "title") {
+    console.log(hit);
+    window.hit = hit;
+    console.log(fieldName);
+    window.fieldName = fieldName;
+  }
+  if (
+    !hit.highlight ||
+    !hit.highlight[fieldName] ||
+    hit.highlight[fieldName].length < 1
+  ) {
+    return;
+  }
+
+  return hit.highlight[fieldName][0];
+}
+
 function getResults(hits) {
-  const toObjectWithRaw = value => ({ raw: value });
+  const addEachKeyValueToObject = (acc, [key, value]) => ({
+    ...acc,
+    [key]: value
+  });
+
+  const toObject = (value, snippet) => {
+    return { raw: value, ...(snippet && { snippet }) };
+  };
 
   return hits.map(record => {
     return Object.entries(record._source)
       .map(([fieldName, fieldValue]) => [
         fieldName,
-        toObjectWithRaw(fieldValue)
+        toObject(fieldValue, getHighlight(record, fieldName))
       ])
       .reduce(addEachKeyValueToObject, {});
   });
 }
 
+function getValueFacet(aggregations, fieldName) {
+  if (
+    aggregations &&
+    aggregations[fieldName] &&
+    aggregations[fieldName].buckets &&
+    aggregations[fieldName].buckets.length > 0
+  ) {
+    return [
+      {
+        field: fieldName,
+        type: "value",
+        data: aggregations[fieldName].buckets.map(bucket => ({
+          // Boolean values and date values require using `key_as_string`
+          value: bucket.key_as_string || bucket.key,
+          count: bucket.doc_count
+        }))
+      }
+    ];
+  }
+}
+
+function getRangeFacet(aggregations, fieldName) {
+  if (
+    aggregations &&
+    aggregations[fieldName] &&
+    aggregations[fieldName].buckets &&
+    aggregations[fieldName].buckets.length > 0
+  ) {
+    return [
+      {
+        field: fieldName,
+        type: "range",
+        data: aggregations[fieldName].buckets.map(bucket => ({
+          // Boolean values and date values require using `key_as_string`
+          value: {
+            to: bucket.to,
+            from: bucket.from,
+            name: bucket.key
+          },
+          count: bucket.doc_count
+        }))
+      }
+    ];
+  }
+}
+
+function getFacets(aggregations) {
+  const states = getValueFacet(aggregations, "states");
+  const world_heritage_site = getValueFacet(
+    aggregations,
+    "world_heritage_site"
+  );
+  const visitors = getRangeFacet(aggregations, "visitors");
+  const acres = getRangeFacet(aggregations, "acres");
+
+  const facets = {
+    ...(states && { states }),
+    ...(world_heritage_site && { world_heritage_site }),
+    ...(visitors && { visitors }),
+    ...(acres && { acres })
+  };
+
+  if (Object.keys(facets).length > 0) {
+    return facets;
+  }
+}
+
 export default function buildState(response, resultsPerPage) {
   const results = getResults(response.hits.hits);
   const info = getInfo(response.hits, resultsPerPage);
+  const facets = getFacets(response.aggregations);
+
   return {
     results,
-    ...info
+    ...info,
+    ...(facets && { facets })
   };
 }
