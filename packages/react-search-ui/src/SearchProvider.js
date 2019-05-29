eb863bf6f3fd2df909c4de486aa7efa03e5b0852
Jason Stoltzfus <jastoltz24@gmail.com>
2019-05-29 14:24:58 -0400

    Additional comments

--- packages/react-search-ui/src/SearchProvider.js
+++ packages/react-search-ui/src/SearchProvider.js
@@ -1,20 +1,16 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
-import { SearchDriver } from "@elastic/search-ui";
 
+import { SearchDriver } from "@elastic/search-ui";
 import SearchContext from "./SearchContext";
 
 /**
  * The SearchProvider primarily holds a reference to the SearchDriver and
  * exposes it to the rest of the application in a Context.
- *
- * Any "Container" component nested within this component will have access
- * to the Driver's state and Actions.
- *
  */
 class SearchProvider extends Component {
   static propTypes = {
-    children: PropTypes.node,
+    children: PropTypes.node.isRequired,
     // Not providing a shape here because the shape matches the shape of
     // SearchDriver. SearchDriver can do it's own parameter validation.
     config: PropTypes.object
@@ -29,27 +25,36 @@ class SearchProvider extends Component {
 
   componentDidMount() {
     const { config } = this.props;
-    this.driver = new SearchDriver(config);
+    // This initialization is done inside of componentDidMount, because initializing the SearchDriver server side
+    // will error out, since the driver depends on window. Placing the initialization inside of componentDidMount
+    // assures that it won't attempt to initialize server side.
+    const driver = new SearchDriver(config);
     this.setState({
-      driver: this.driver
+      driver
     });
   }
 
   componentWillUnmount() {
-    this.driver.tearDown();
+    this.state.driver.tearDown();
   }
 
   render() {
     const { children } = this.props;
 
+    // Since driver is initialized in componentDidMount above, we are waiting
+    // to render until the driver is available.
     if (!this.state.driver) return null;
 
-    const providerValue = {
-      driver: this.state.driver
-    };
-
+    // Passing the entire "this.state" to the Context is significant. Because
+    // Context determines when to re-render based on referential identity
+    // something like this could cause unnecessary renders:
+    //
+    // <SearchContext.Provider value={{driver: this.state.driver}}>
+    //
+    // By passing the entire state, we ensure that re-renders only occur when
+    // state is actually updated.
     return (
-      <SearchContext.Provider value={providerValue}>
+      <SearchContext.Provider value={this.state}>
         {children}
       </SearchContext.Provider>
     );
