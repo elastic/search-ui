3dd4a8b6a72eb396e7ce41a5a48caa854d7d3bba
Jason Stoltzfus <jastoltz24@gmail.com>
2019-06-24 16:41:12 -0400

    Update packages/react-search-ui/src/types/Result.js
    Co-Authored-By: Constance <constancecchen@users.noreply.github.com>

--- packages/react-search-ui/src/types/Result.js
+++ packages/react-search-ui/src/types/Result.js
@@ -5,7 +5,7 @@ import FieldValueWrapper from "./FieldValueWrapper";
 // Typically an object where keys are the field names, and values are field values.
 // Also could be literally any other arbitrary value depending on the particular Search API response.
 // Default views in Search UI know what to do with FieldValueWrapper values, but not arbitrary values, so it
-// is typically better to work with FieldValueWrapper values. So we encourage FieldValueWrapper, but we accept
+// is usually better to work with FieldValueWrapper values. We encourage FieldValueWrapper, but we accept
 // anything because we don't want users to have type error warnings unnecessarily.
 //
 // An example would be if a user requests "grouping" in an App Search API request. That will come back
