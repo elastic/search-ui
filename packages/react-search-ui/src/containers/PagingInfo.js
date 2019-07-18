46fdf992c6cf092b8c08c19d38aab6deb51bde06
Constance Chen <constance.chen.3@gmail.com>
2019-07-18 14:08:59 -0700

    Revert PagingInfoContainer changes

--- packages/react-search-ui/src/containers/PagingInfo.js
+++ packages/react-search-ui/src/containers/PagingInfo.js
@@ -13,16 +13,12 @@ export class PagingInfoContainer extends Component {
     results: PropTypes.arrayOf(PropTypes.object).isRequired,
     resultsPerPage: PropTypes.number.isRequired,
     resultSearchTerm: PropTypes.string.isRequired,
-    totalResults: PropTypes.number.isRequired,
-    a11yNotify: PropTypes.func.isRequired,
-    a11yNotificationMessages: PropTypes.objectOf(PropTypes.func)
+    totalResults: PropTypes.number.isRequired
   };
 
   render() {
     const {
       className,
-      a11yNotificationMessages,
-      a11yNotify,
       current,
       resultsPerPage,
       resultSearchTerm,
@@ -31,40 +27,28 @@ export class PagingInfoContainer extends Component {
     } = this.props;
     const start = totalResults === 0 ? 0 : (current - 1) * resultsPerPage + 1;
     const end =
-      totalResults <= start + resultsPerPage
+      totalResults <= resultsPerPage
         ? totalResults
         : start + resultsPerPage - 1;
 
-    const resultsProps = {
+    const View = view || PagingInfo;
+
+    return View({
+      className,
       end: end,
       searchTerm: resultSearchTerm,
       start: start,
       totalResults: totalResults
-    };
-
-    a11yNotify(a11yNotificationMessages.searchResults(resultsProps));
-
-    const View = view || PagingInfo;
-    return View({ ...resultsProps, className });
+    });
   }
 }
 
 export default withSearch(
-  ({
+  ({ current, results, resultsPerPage, resultSearchTerm, totalResults }) => ({
     current,
     results,
     resultsPerPage,
     resultSearchTerm,
-    totalResults,
-    a11yNotificationMessages,
-    a11yNotify
-  }) => ({
-    current,
-    results,
-    resultsPerPage,
-    resultSearchTerm,
-    totalResults,
-    a11yNotificationMessages,
-    a11yNotify
+    totalResults
   })
 )(PagingInfoContainer);
