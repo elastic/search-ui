78c45d0ae324562fc4edb2edec92b6e62f031bec
Constance <constancecchen@users.noreply.github.com>
2019-07-18 12:44:55 -0700

    More robust fix for PagingInfo showing a higher end than possible (#354)
    * Revert previous Math.min() logic
    
    * Move greater than totalResults check to PagingInfoContainer

--- packages/react-search-ui/src/containers/__tests__/PagingInfo.test.js
+++ packages/react-search-ui/src/containers/__tests__/PagingInfo.test.js
@@ -37,6 +37,20 @@ it("renders when it doesn't have any results or a result search term", () => {
   expect(wrapper).toMatchSnapshot();
 });
 
+it("does not render more than the total # of results", () => {
+  const wrapper = shallow(<PagingInfoContainer {...params} totalResults={5} />);
+  expect(wrapper.text()).toEqual("Showing 1 - 5 out of 5 for: test");
+
+  wrapper.setProps({ current: 3, resultsPerPage: 5, totalResults: 12 });
+  expect(wrapper.text()).toEqual("Showing 11 - 12 out of 12 for: test");
+
+  wrapper.setProps({ totalResults: 15 });
+  expect(wrapper.text()).toEqual("Showing 11 - 15 out of 15 for: test");
+
+  wrapper.setProps({ totalResults: 0 });
+  expect(wrapper.text()).toEqual("Showing 0 - 0 out of 0 for: test");
+});
+
 it("passes className through to the view", () => {
   let viewProps;
   const className = "test-class";
