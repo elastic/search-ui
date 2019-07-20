8bddf062a37ac457463d9ca4ec12e29b4214e6cd
Kaushik ASP <aspkaushik@gmail.com>
2019-07-20 19:41:59 +0530

    Removes text indicating the searchTerm from PagingInfo

--- packages/react-search-ui-views/src/PagingInfo.js
+++ packages/react-search-ui-views/src/PagingInfo.js
@@ -3,14 +3,14 @@ import React from "react";
 
 import { appendClassName } from "./view-helpers";
 
-function PagingInfo({ className, end, searchTerm, start, totalResults }) {
+function PagingInfo({ className, end, start, totalResults }) {
   return (
     <div className={appendClassName("sui-paging-info", className)}>
       Showing{" "}
       <strong>
         {start} - {end}
       </strong>{" "}
-      out of <strong>{totalResults}</strong> for: <em>{searchTerm}</em>
+      out of <strong>{totalResults}</strong>
     </div>
   );
 }
