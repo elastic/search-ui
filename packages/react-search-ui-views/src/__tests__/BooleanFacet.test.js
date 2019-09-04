cc9d1de7f23a472fadb83e0e8ee88bf8c2c52682
Jose Felix <jfelx82@gmail.com>
2019-09-04 00:26:05 -0400

    Update unit tests

--- packages/react-search-ui-views/src/__tests__/BooleanFacet.test.js
+++ packages/react-search-ui-views/src/__tests__/BooleanFacet.test.js
@@ -20,47 +20,11 @@ it("renders", () => {
   expect(wrapper).toMatchSnapshot();
 });
 
-describe("determine selection", () => {
-  it("will show count when Facet is not selected", () => {
-    const wrapper = shallow(<BooleanFacet {...params} />);
-
-    expect(wrapper.exists("span")).toBe(false);
-  });
-
-  it("will show X when Facet is selected", () => {
-    const wrapper = shallow(<BooleanFacet {...params} />);
-    wrapper.setProps({ values: ["true"] });
-
-    expect(wrapper.exists("span")).toBe(true);
-  });
-
-  it("will show count once Facet is clicked twice", () => {
-    const wrapper = shallow(<BooleanFacet {...params} />);
-
-    //First click
-    wrapper.setProps({ values: ["true"] });
-
-    expect(wrapper.exists("span")).toBe(true);
-
-    //Second click
-    wrapper.setProps({ values: [] });
-    expect(wrapper.exists("span")).toBe(false);
-  });
-
-  it("shows class 'selected' when Facet is selected", () => {
-    const wrapper = shallow(<BooleanFacet {...params} />);
-
-    wrapper.setProps({ values: ["true"] });
-
-    expect(wrapper.find("div").props().className).toEqual("selected");
-  });
-});
-
 it("renders with className prop applied", () => {
   const customClassName = "test-class";
   const wrapper = shallow(
     <BooleanFacet className={customClassName} {...params} />
   );
   const { className } = wrapper.props();
-  expect(className).toEqual("test-class");
+  expect(className.includes(customClassName)).toBe(true);
 });
