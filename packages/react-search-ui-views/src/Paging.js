e224de92b4128a4c04690af9ad427fc4cf6278a6
Vadim Yakhin <yakhin.v@gmail.com>
2019-10-07 09:57:04 -0300

    Fix pagination locale

--- packages/react-search-ui-views/src/Paging.js
+++ packages/react-search-ui-views/src/Paging.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import React from "react";
 import RCPagination from "rc-pagination";
+import enUsLocale from "rc-pagination/lib/locale/en_US";
 
 import { appendClassName } from "./view-helpers";
 
@@ -19,6 +20,7 @@ function Paging({
       pageSize={resultsPerPage}
       total={totalPages * resultsPerPage}
       className={appendClassName("sui-paging", className)}
+      locale={enUsLocale}
       {...rest}
     />
   );
