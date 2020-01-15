c2c51160470536f95d3db0a3545b4bc11f86483f
Jason Stoltzfus <jastoltz24@gmail.com>
2020-01-15 13:12:04 -0500

    Removed comments and changed types to object

--- packages/search-ui-app-search-connector/search-ui-app-search-connector.d.ts
+++ packages/search-ui-app-search-connector/search-ui-app-search-connector.d.ts
@@ -1,32 +1,5 @@
 declare module "@elastic/search-ui-app-search-connector" {
   class AppSearchAPIConnector {
-    /**
-     * @typedef {Object.<string, *>} KeyValuePairs
-     */
-    /**
-     * @callback next
-     * @param {KeyValuePairs} updatedQueryOptions The options to send to the API
-     * @returns {Object} The response
-     */
-    /**
-     * @callback hook
-     * @param {KeyValuePairs} queryOptions The options that are about to be sent to the API
-     * @param {next} next The options that are about to be sent to the API
-     */
-    /**
-     * @param {Object} options Credential found in your App Search Dashboard
-     * @param {string} options.searchKey Credential found in your App Search Dashboard
-     * @param {string} options.engineName Engine to query, found in your App Search Dashboard
-     * @param {string=} options.hostIdentifier Credential found in your App Search Dashboard. Required only if not using
-     * the endpointBase option.
-     * @param {hook=} options.beforeSearchCall A hook to amend request or response to API for "onSearch" event.
-     * @param {hook=} options.beforeAutocompleteResultsCall A hook to amend request or response to API for a "results"
-     * query on an "onAutocomplete" event.
-     * @param {hook=} options.beforeAutocompleteSuggestionsCall A hook to amend request or response to API for a "suggestions"
-     * query on an "onAutocomplete" event.
-     * @param {string=} options.endpointBase="" Overrides the base of the Swiftype API endpoint completely. Useful
-     * for non-SaaS App Search deployments. For example, Elastic Cloud, Self-Managed, or proxying SaaS.
-     */
     constructor({
       searchKey,
       engineName,
@@ -40,22 +13,16 @@ declare module "@elastic/search-ui-app-search-connector" {
       engineName: string;
       hostIdentifier?: string;
       beforeSearchCall?: (
-        queryOptions: {
-          [x: string]: any;
-        },
-        next: (updatedQueryOptions: { [x: string]: any }) => any
+        queryOptions: object,
+        next: (updatedQueryOptions: object) => any
       ) => any;
       beforeAutocompleteResultsCall?: (
-        queryOptions: {
-          [x: string]: any;
-        },
-        next: (updatedQueryOptions: { [x: string]: any }) => any
+        queryOptions: object,
+        next: (updatedQueryOptions: object) => any
       ) => any;
       beforeAutocompleteSuggestionsCall?: (
-        queryOptions: {
-          [x: string]: any;
-        },
-        next: (updatedQueryOptions: { [x: string]: any }) => any
+        queryOptions: object,
+        next: (updatedQueryOptions: object) => any
       ) => any;
       endpointBase?: string;
     });
