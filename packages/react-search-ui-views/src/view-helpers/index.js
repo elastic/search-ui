502aee89d8d550d2e87f02a07bd505550e06c0cc
Jason Stoltzfus <jastoltz24@gmail.com>
2020-04-15 11:12:43 -0400

    Additional checks on Result component (#481) (#482)

--- packages/react-search-ui-views/src/view-helpers/index.js
+++ packages/react-search-ui-views/src/view-helpers/index.js
@@ -1,2 +1,3 @@
 export { default as getFilterValueDisplay } from "./getFilterValueDisplay";
 export { default as appendClassName } from "./appendClassName";
+export { default as getUrlSanitizer } from "./getUrlSanitizer";
