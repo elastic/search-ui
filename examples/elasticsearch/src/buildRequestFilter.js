860a5711130a59041df217808e6fe3ddd1c8453e
heeyounglee <heeyoung@roa.ai>
2020-05-27 11:43:19 +0900

    Remove unnecessary array brackets invoking parsing errors in BuildRequestFilter.js

--- examples/elasticsearch/src/buildRequestFilter.js
+++ examples/elasticsearch/src/buildRequestFilter.js
@@ -15,22 +15,18 @@ function getTermFilter(filter) {
   if (filter.type === "any") {
     return {
       bool: {
-        should: [
-          filter.values.map(filterValue => ({
+        should: filter.values.map(filterValue => ({
             term: getTermFilterValue(filter.field, filterValue)
-          }))
-        ],
+          })),
         minimum_should_match: 1
       }
     };
   } else if (filter.type === "all") {
     return {
       bool: {
-        filter: [
-          filter.values.map(filterValue => ({
+        filter: filter.values.map(filterValue => ({
             term: getTermFilterValue(filter.field, filterValue)
           }))
-        ]
       }
     };
   }
@@ -40,24 +36,21 @@ function getRangeFilter(filter) {
   if (filter.type === "any") {
     return {
       bool: {
-        should: [
-          filter.values.map(filterValue => ({
+        should: filter.values.map(filterValue => ({
             range: {
               [filter.field]: {
                 ...(filterValue.to && { lt: filterValue.to }),
                 ...(filterValue.to && { gt: filterValue.from })
               }
             }
-          }))
-        ],
+          })),
         minimum_should_match: 1
       }
     };
   } else if (filter.type === "all") {
     return {
       bool: {
-        filter: [
-          filter.values.map(filterValue => ({
+        filter: filter.values.map(filterValue => ({
             range: {
               [filter.field]: {
                 ...(filterValue.to && { lt: filterValue.to }),
@@ -65,7 +58,6 @@ function getRangeFilter(filter) {
               }
             }
           }))
-        ]
       }
     };
   }
