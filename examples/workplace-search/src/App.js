63a67f60f22b5a2cbc5abc4679d2ca191b1982f1
Vadim Yakhin <vadim.yakhin@elastic.co>
2022-01-26 10:48:32 -0800

    Replace app-search-connector with workplace-search-connector, clean up

--- examples/workplace-search/src/App.js
+++ examples/workplace-search/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
-import SiteSearchAPIConnector from "@elastic/search-ui-site-search-connector";
+import WorkplaceSearchAPIConnector from "@elastic/search-ui-workplace-search-connector";
+
 import {
   ErrorBoundary,
   SearchProvider,
@@ -14,24 +14,11 @@ import {
 } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
 
-let connector;
-if (process.env.REACT_APP_SOURCE === "SITE_SEARCH") {
-  connector = new SiteSearchAPIConnector({
-    engineKey:
-      process.env.REACT_SITE_SEARCH_ENGINE_KEY || "Z43R5U3HiDsDgpKawZkA",
-    documentType: process.env.REACT_SITE_SEARCH_ENGINE_NAME || "national-parks"
-  });
-} else {
-  connector = new AppSearchAPIConnector({
-    searchKey:
-      process.env.REACT_APP_SEARCH_KEY || "search-371auk61r2bwqtdzocdgutmg",
-    engineName:
-      process.env.REACT_APP_SEARCH_ENGINE_NAME || "search-ui-examples",
-    hostIdentifier:
-      process.env.REACT_APP_SEARCH_HOST_IDENTIFIER || "host-2376rb",
-    endpointBase: process.env.REACT_APP_SEARCH_ENDPOINT_BASE || ""
-  });
-}
+const connector = new WorkplaceSearchAPIConnector({
+  endpointBase: "http://localhost:3002",
+  redirectUri: "http://localhost:3000",
+  clientId: "ace9c82ba444dafb593250e483dc66b24713f2e6e70bf4bc2eaafc5c682d45cd",
+})
 
 const config = {
   alwaysSearchOnInitialLoad: true,
--- packages/search-ui-workplace-search-connector/src/AppSearchAPIConnector.js
+++ packages/search-ui-workplace-search-connector/src/AppSearchAPIConnector.js
@@ -1,5 +1,3 @@
-import * as ElasticAppSearch from "@elastic/app-search-javascript";
-
 import { adaptResponse } from "./responseAdapter";
 import { adaptRequest } from "./requestAdapters";
 import buildResponseAdapterOptions from "./buildResponseAdapterOptions";
@@ -14,73 +12,11 @@ function removeEmptyFacetsAndFilters(options) {
     ...rest
   };
 }
-class AppSearchAPIConnector {
-  /**
-   * @callback next
-   * @param {Object} updatedQueryOptions The options to send to the API
-   */
-
-  /**
-   * @callback hook
-   * @param {Object} queryOptions The options that are about to be sent to the API
-   * @param {next} next The options that are about to be sent to the API
-   */
-
-  /**
-   * @typedef Options
-   * @param {string} searchKey Credential found in your App Search Dashboard
-   * @param {string} engineName Engine to query, found in your App Search Dashboard
-   * @param {string} hostIdentifier Credential found in your App Search Dashboard
-   *  Useful when proxying the Swiftype API or developing against a local API server.
-   * @param {hook} beforeSearchCall=(queryOptions,next)=>next(queryOptions) A hook to amend query options before the request is sent to the
-   *   API in a query on an "onSearch" event.
-   * @param {hook} beforeAutocompleteResultsCall=(queryOptions,next)=>next(queryOptions) A hook to amend query options before the request is sent to the
-   *   API in a "results" query on an "onAutocomplete" event.
-   * @param {hook} beforeAutocompleteSuggestionsCall=(queryOptions,next)=>next(queryOptions) A hook to amend query options before the request is sent to
-   * the API in a "suggestions" query on an "onAutocomplete" event.
-   * @param {string} endpointBase="" Overrides the base of the Swiftype API endpoint completely.
-   */
-
-  /**
-   * @param {Options} options
-   */
-  constructor({
-    searchKey,
-    engineName,
-    hostIdentifier,
-    beforeSearchCall = (queryOptions, next) => next(queryOptions),
-    beforeAutocompleteResultsCall = (queryOptions, next) => next(queryOptions),
-    beforeAutocompleteSuggestionsCall = (queryOptions, next) =>
-      next(queryOptions),
-    endpointBase = "",
-    ...rest
-  }) {
-    if (!engineName || !(hostIdentifier || endpointBase)) {
-      throw Error(
-        "hostIdentifier or endpointBase, and engineName are required"
-      );
-    }
-
-    this.client = ElasticAppSearch.createClient({
-      ...(endpointBase && { endpointBase }), //Add property on condition
-      ...(hostIdentifier && { hostIdentifier: hostIdentifier }),
-      apiKey: searchKey,
-      engineName: engineName,
-      ...rest
-    });
-    this.beforeSearchCall = beforeSearchCall;
-    this.beforeAutocompleteResultsCall = beforeAutocompleteResultsCall;
-    this.beforeAutocompleteSuggestionsCall = beforeAutocompleteSuggestionsCall;
-  }
-
-  onResultClick({ query, documentId, requestId, tags = [] }) {
-    tags = tags.concat("results");
-    return this.client.click({ query, documentId, requestId, tags });
-  }
-
-  onAutocompleteResultClick({ query, documentId, requestId, tags = [] }) {
-    tags = tags.concat("autocomplete");
-    return this.client.click({ query, documentId, requestId, tags });
+class WorkplaceSearchAPIConnector {
+  constructor({ clientId, redirectUri, endpointBase = "" }) {
+    this.clientId = clientId;
+    this.redirectUri = redirectUri;
+    this.endpointBase = endpointBase;
   }
 
   async onSearch(state, queryConfig) {
@@ -117,67 +53,6 @@ class AppSearchAPIConnector {
       return adaptResponse(response, buildResponseAdapterOptions(queryConfig));
     });
   }
-
-  async onAutocomplete({ searchTerm }, queryConfig) {
-    const autocompletedState = {};
-    let promises = [];
-
-    if (queryConfig.results) {
-      const {
-        current,
-        filters,
-        resultsPerPage,
-        sortDirection,
-        sortField,
-        sortList,
-        ...restOfQueryConfig
-      } = queryConfig.results;
-
-      const { query, ...optionsFromState } = adaptRequest({
-        current,
-        searchTerm,
-        filters,
-        resultsPerPage,
-        sortDirection,
-        sortField,
-        sortList
-      });
-
-      const withQueryConfigOptions = {
-        ...restOfQueryConfig,
-        ...optionsFromState
-      };
-      const options = removeEmptyFacetsAndFilters(withQueryConfigOptions);
-      promises.push(
-        this.beforeAutocompleteResultsCall(options, newOptions => {
-          return this.client.search(query, newOptions).then(response => {
-            autocompletedState.autocompletedResults = adaptResponse(
-              response
-            ).results;
-            autocompletedState.autocompletedResultsRequestId =
-              response.info.meta.request_id;
-          });
-        })
-      );
-    }
-
-    if (queryConfig.suggestions) {
-      const options = queryConfig.suggestions;
-
-      promises.push(
-        this.beforeAutocompleteSuggestionsCall(options, newOptions =>
-          this.client.querySuggestion(searchTerm, newOptions).then(response => {
-            autocompletedState.autocompletedSuggestions = response.results;
-            autocompletedState.autocompletedSuggestionsRequestId =
-              response.meta.request_id;
-          })
-        )
-      );
-    }
-
-    await Promise.all(promises);
-    return autocompletedState;
-  }
 }
 
-export default AppSearchAPIConnector;
+export default WorkplaceSearchAPIConnector;
