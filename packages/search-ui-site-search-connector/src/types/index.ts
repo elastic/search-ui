9e56d4bf5a9eff52c125d2e3539c06420fd6f755
Vadim Yakhin <vadim.yakhin@elastic.co>
2022-02-14 15:33:59 -0800

    Add types file

--- packages/search-ui-site-search-connector/src/types/index.ts
+++ packages/search-ui-site-search-connector/src/types/index.ts
@@ -1,6 +1,41 @@
+import type {
+  FieldConfiguration,
+  FacetConfiguration
+} from "@elastic/search-ui";
+
 export type SearchResponse = {
   record_count?: number;
   records?: Record<string, any>;
   info?: Record<string, any>;
   errors?: Record<string, string[]>;
+};
+
+type Boost = "logarithmic" | "linear" | "exponential";
+
+export type SiteSearchQueryConfig = {
+  resultsPerPage?: number;
+  current?: number;
+  sortDirection?: Record<string, "asc" | "desc">;
+  sortField?: Record<string, string>;
+  sortList?: any;
+  result_fields?: Record<string, FieldConfiguration>;
+  search_fields?: Record<string, string[]>;
+  facets?: Record<string, string[] | FacetConfiguration>; // which type is it? Tests conflict with docs.
+  // extra params not handled by requestAdapter but available on API
+  spelling?: "strict" | "always" | "retry";
+  functional_boosts?: Record<string, Record<string, Boost>>;
+  document_types?: string[];
+  [key: string]: any;
+};
+
+export type SearchQueryHook = (
+  queryOptions: SiteSearchQueryConfig,
+  next: (newQueryOptions: SiteSearchQueryConfig) => any
+) => any;
+
+export type SiteSearchAPIConnectorParams = {
+  documentType: string;
+  engineKey: string;
+  beforeSearchCall?: SearchQueryHook;
+  beforeAutocompleteResultsCall?: SearchQueryHook;
 };
