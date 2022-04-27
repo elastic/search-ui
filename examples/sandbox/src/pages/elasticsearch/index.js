2573e938828c0ed73390627890316a4f123de847
Vadim Yakhin <vadim.yakhin@elastic.co>
2022-04-27 11:53:07 -0700

    Use Cloud data in App Search and Elasticsearch sandboxes (#726)

--- examples/sandbox/src/pages/elasticsearch/index.js
+++ examples/sandbox/src/pages/elasticsearch/index.js
@@ -26,8 +26,9 @@ import "@elastic/react-search-ui-views/lib/styles/styles.css";
 
 const connector = new ElasticSearchAPIConnector(
   {
-    host: process.env.REACT_ELASTICSEARCH_HOST || "http://localhost:9200",
-    index: process.env.REACT_ELASTICSEARCH_INDEX || "us_parks"
+    host: process.env.REACT_ELASTICSEARCH_HOST || "https://search-ui-sandbox.es.us-central1.gcp.cloud.es.io:9243",
+    index: process.env.REACT_ELASTICSEARCH_INDEX || "national-parks",
+    apiKey: process.env.REACT_ELASTICSEARCH_API_KEY ||"SlUzdWE0QUJmN3VmYVF2Q0F6c0I6TklyWHFIZ3lTbHF6Yzc2eEtyeWFNdw=="
   }
 );
 
