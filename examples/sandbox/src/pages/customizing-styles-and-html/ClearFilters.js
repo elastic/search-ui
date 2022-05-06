15b321e42ada00828bf71e2cf94e5e4ede833939
Vadim Yakhin <vadim.yakhin@elastic.co>
2022-05-06 11:54:44 -0700

    Add new example "customizing-styles-and-html"

---
+++ examples/sandbox/src/pages/customizing-styles-and-html/ClearFilters.js
@@ -0,0 +1,16 @@
+import { withSearch } from "@elastic/react-search-ui";
+
+function ClearFilters({ filters, clearFilters }) {
+  return (
+    <div>
+      <button onClick={() => clearFilters()}>
+        Clear {filters.length} Filters
+      </button>
+    </div>
+  );
+}
+
+export default withSearch(({ filters, clearFilters }) => ({
+  filters,
+  clearFilters
+}))(ClearFilters);
