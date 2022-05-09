df684eba43a144eeed73679e9358383a3cd76fda
Joseph McElroy <joseph.mcelroy@elastic.co>
2022-05-09 19:29:38 +0100

    [Elasticsearch-connector] Autocomplete suggestions (#743)
    * use searchkit suggestions feature for elasticsearch
    
    * Merge branch 'master' of github.com:elastic/search-ui into autocomplete
    
    * documentation + make hits size configurable

--- packages/search-ui-elasticsearch-connector/src/handlers/autocomplete/suggestions.ts
+++
@@ -1,6 +0,0 @@
-import { RequestState } from "@elastic/search-ui";
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function handler(state: RequestState, suggestionsConfig: any) {
-  return Promise.resolve([]);
-}
