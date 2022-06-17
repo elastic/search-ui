a6e17c9b958f9a82e5376a0f3ea8d52a06badf93
Vadim Yakhin <vadim.yakhin@elastic.co>
2022-06-17 10:18:09 -0700

    Add APM RUM guide to documentation

--- examples/sandbox/src/Router.js
+++ examples/sandbox/src/Router.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Root from "./pages/root";
 import Elasticsearch from "./pages/elasticsearch";
 import AppSearch from "./pages/app-search";
@@ -9,37 +9,38 @@ import SearchAsYouType from "./pages/search-as-you-type";
 import CustomizingStylesAndHtml from "./pages/customizing-styles-and-html";
 import SearchBarInHeaderIndex from "./pages/search-bar-in-header/index";
 import SearchBarInHeaderSearch from "./pages/search-bar-in-header/search";
-import { ApmRoute } from "@elastic/apm-rum-react";
 
 export default function Router() {
   return (
     <div className="App">
       <Switch>
-        <ApmRoute exact path="/" component={Root} />
-        <ApmRoute exact path="/elasticsearch" component={Elasticsearch} />
-        <ApmRoute exact path="/app-search" component={AppSearch} />
-        <ApmRoute exact path="/site-search" component={SiteSearch} />
-        <ApmRoute exact path="/workplace-search" component={WorkplaceSearch} />
-        <ApmRoute
-          exact
-          path="/search-as-you-type"
-          component={SearchAsYouType}
-        />
-        <ApmRoute
-          exact
-          path="/customizing-styles-and-html"
-          component={CustomizingStylesAndHtml}
-        />
-        <ApmRoute
-          exact
-          path="/search-bar-in-header"
-          component={SearchBarInHeaderIndex}
-        />
-        <ApmRoute
-          exact
-          path="/search-bar-in-header/search"
-          component={SearchBarInHeaderSearch}
-        />
+        <Route exact path="/">
+          <Root />
+        </Route>
+        <Route exact path="/elasticsearch">
+          <Elasticsearch />
+        </Route>
+        <Route exact path="/app-search">
+          <AppSearch />
+        </Route>
+        <Route exact path="/site-search">
+          <SiteSearch />
+        </Route>
+        <Route exact path="/workplace-search">
+          <WorkplaceSearch />
+        </Route>
+        <Route exact path="/search-as-you-type">
+          <SearchAsYouType />
+        </Route>
+        <Route exact path="/customizing-styles-and-html">
+          <CustomizingStylesAndHtml />
+        </Route>
+        <Route exact path="/search-bar-in-header">
+          <SearchBarInHeaderIndex />
+        </Route>
+        <Route exact path="/search-bar-in-header/search">
+          <SearchBarInHeaderSearch />
+        </Route>
       </Switch>
     </div>
   );
