299376d5e74c8e369adb48c58a2648b08b333fee
Vadim Yakhin <vadim.yakhin@elastic.co>
2022-07-04 13:37:05 -0700

    Replace template files with actual app files

---
+++ examples/vue3/src/components/SearchHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="sui-layout-header">
+    <div class="sui-layout-header__inner">
+      <form @submit.prevent="$emit('submit', $event.target.value)">
+        <div class="sui-search-box">
+          <div class="sui-search-box__wrapper">
+            <input
+              type="text"
+              placeholder="Try searching for 'water', 'dragon' or 'ragnaros'"
+              class="sui-search-box__text-input"
+              :value="value"
+              @input="$emit('input', $event.target.value)"
+            />
+          </div>
+          <input
+            type="submit"
+            class="button sui-search-box__submit"
+            value="Search"
+          />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      required: true,
+      type: String
+    }
+  }
+};
+</script>
---
+++ examples/vue3/src/components/SearchSort.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="sui-sorting">
+    <div class="sui-sorting__label">Sort by</div>
+    <select
+      name="sortBy"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    >
+      <option value="">Relevance</option>
+      <option value="name">Name</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
