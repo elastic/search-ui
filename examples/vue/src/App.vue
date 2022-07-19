120d3b5419a46b7325cf4175efccdb17c399ee06
Vadim Yakhin <vadim.yakhin@elastic.co>
2022-07-19 10:33:47 -0700

    Add Vue.js example (#805)
    * Add start-docs command
    
    * Copy the https://github.com/elastic/vue-search-ui-demo to examples
    
    * Install deps and fix errors
    
    * Run prettier
    
    * Upgrade to Vue 3
    
    * Scaffold vue3 app with `npm init vue@latest`
    
    * Replace template files with actual app files
    
    * Delete old app
    
    * Rename vue3 folder to vue
    
    * Add metadata to package.json
    
    * Update credentials to use cloud deployment and update data
    
    * Disable pagination since it's not supported by Vue3
    
    * Update yarn file
    
    * Add start-vue command
    
    * Disable pagination package since it's not working on Vue3
    
    * Fix broken inputs
    
    By using this guide: https://vuejs.org/guide/components/events.html#usage-with-v-model
    
    * Add missing imports
    
    * Update path to css file
    
    * Add Vue.js guide

---
+++ examples/vue/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1 class="header">Search UI demo with Vue (Hearthstone cards)</h1>
+    <SearchSection />
+  </div>
+</template>
+
+<script>
+import SearchSection from "./components/SearchSection.vue";
+
+export default {
+  components: {
+    SearchSection
+  }
+};
+</script>
+
+<style>
+@import "../node_modules/@elastic/react-search-ui-views/lib/styles/styles.css";
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.header {
+  text-align: center;
+}
+</style>
