76feae8d5b95565ae69586b4e7260fd2fa4236ea
Vadim Yakhin <vadim.yakhin@elastic.co>
2022-07-19 10:33:47 -0700

    Add Vue.js example (#805)
    * Add start-docs command
    
    * Copy the https://github.com/elastic/vue-search-ui-demo to examples
    
    * Install deps and fix errors
    
    * Run prettier
    
    * Upgrade to Vue 3
    
    * Scaffold vue3 app with `npm init vue@latest`
    
    * Replace template files with actual app files
    
    * Delete old app
    
    * Rename vue3 folder to vue
    
    * Add metadata to package.json
    
    * Update credentials to use cloud deployment and update data
    
    * Disable pagination since it's not supported by Vue3
    
    * Update yarn file
    
    * Add start-vue command
    
    * Disable pagination package since it's not working on Vue3
    
    * Fix broken inputs
    
    By using this guide: https://vuejs.org/guide/components/events.html#usage-with-v-model
    
    * Add missing imports
    
    * Update path to css file
    
    * Add Vue.js guide

---
+++ examples/vue/src/components/SearchSort.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="sui-sorting">
+    <div class="sui-sorting__label">Sort by</div>
+    <select
+      name="sortBy"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    >
+      <option value="">Relevance</option>
+      <option value="name">Name</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["modelValue"],
+  emits: ["update:modelValue"]
+};
+</script>
