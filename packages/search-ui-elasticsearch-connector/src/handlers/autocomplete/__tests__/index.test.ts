8f8a0416d3e496af169a6bee4cd7970b21817336
Joseph McElroy <joseph.mcelroy@elastic.co>
2022-07-22 18:06:45 +0100

    elasticsearch-connector: autocomplete support from another index

--- packages/search-ui-elasticsearch-connector/src/handlers/autocomplete/__tests__/index.test.ts
+++ packages/search-ui-elasticsearch-connector/src/handlers/autocomplete/__tests__/index.test.ts
@@ -4,8 +4,25 @@ import handleRequest from "../index";
 
 const mockSearchkitResponse = [
   {
-    identifier: "results",
+    identifier: "suggestions-completion-results",
     suggestions: ["sweaters", "sweatpants"]
+  },
+  {
+    identifier: "suggestions-hits-popularQueries",
+    hits: [
+      {
+        id: "acadia",
+        fields: {
+          query: "hello"
+        },
+        highlight: {
+          query: "hello"
+        },
+        rawHit: {
+          _id: "test"
+        }
+      }
+    ]
   },
   {
     identifier: "hits-suggestions",
@@ -67,6 +84,17 @@ const queryConfig: AutocompleteQueryConfig = {
     types: {
       results: {
         fields: ["title"]
+      },
+      popularQueries: {
+        queryType: "results",
+        search_fields: {
+          title: {}
+        },
+        result_fields: {
+          title: {
+            raw: {}
+          }
+        }
       }
     }
   }
@@ -111,6 +139,23 @@ describe("Autocomplete results", () => {
           },
         ],
         "autocompletedSuggestions": Object {
+          "popularQueries": Array [
+            Object {
+              "hit": Object {
+                "_meta": Object {
+                  "id": "test",
+                },
+                "id": Object {
+                  "raw": "acadia",
+                },
+                "query": Object {
+                  "raw": "hello",
+                  "snippet": "hello",
+                },
+              },
+              "queryType": "results",
+            },
+          ],
           "results": Array [
             Object {
               "suggestion": "sweaters",
