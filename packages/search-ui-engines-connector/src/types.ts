62de2d3f1fac0b86f9f1932ddc2c4f6a544d6b65
Joseph McElroy <joseph.mcelroy@elastic.co>
2023-01-30 12:04:00 +0000

    support to customise the engine _search url

--- packages/search-ui-engines-connector/src/types.ts
+++ packages/search-ui-engines-connector/src/types.ts
@@ -2,3 +2,4 @@ import type { estypes } from "@elastic/elasticsearch";
 
 export type SearchRequest = estypes.SearchRequest;
 export type SearchResponse = estypes.SearchResponse<Record<string, unknown>>;
+export type EngineRouteFn = (host: string, engineName: string) => string;
