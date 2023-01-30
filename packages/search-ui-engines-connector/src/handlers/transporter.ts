c982b2180169b8d2e89c45d5c59d7f508913073d
Joseph McElroy <joseph.mcelroy@elastic.co>
2023-01-30 12:04:00 +0000

    support to customise the engine _search url

--- packages/search-ui-engines-connector/src/handlers/transporter.ts
+++ packages/search-ui-engines-connector/src/handlers/transporter.ts
@@ -1,28 +1,30 @@
 import type { SearchkitTransporter } from "@searchkit/sdk";
-import { SearchRequest, SearchResponse } from "../types";
+import { SearchRequest, SearchResponse, EngineRouteFn } from "../types";
+
+function defaultEngineRoute(host: string, engineName: string): string {
+  return `${host}/api/engines/${engineName}/_search`;
+}
 
 export class EngineTransporter implements SearchkitTransporter {
   constructor(
     private host: string,
     private engineName: string,
-    private apiKey: string
+    private apiKey: string,
+    private getRoute: EngineRouteFn = defaultEngineRoute
   ) {}
 
   async performRequest(requestBody: SearchRequest): Promise<SearchResponse> {
     if (!fetch)
       throw new Error("Fetch is not supported in this browser / environment");
 
-    const response = await fetch(
-      this.host + `/api/engines/${this.engineName}/_search`,
-      {
-        method: "POST",
-        body: JSON.stringify(requestBody),
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `ApiKey ${this.apiKey}`
-        }
+    const response = await fetch(this.getRoute(this.host, this.engineName), {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `ApiKey ${this.apiKey}`
       }
-    );
+    });
     const json = await response.json();
     return json;
   }
