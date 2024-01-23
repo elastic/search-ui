51e5d4cc8e2beeb08e39f726f0872e40164303c6
Rodney Norris <rodney.norris@elastic.co>
2024-01-23 21:02:37 +0000

    remove apm init as it is broken

--- examples/sandbox/src/Router.js
+++ examples/sandbox/src/Router.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Root from "./pages/root";
 import Elasticsearch from "./pages/elasticsearch";
 import ElasticsearchWithAnalytics from "./pages/elasticsearch-with-analytics";
@@ -14,54 +14,53 @@ import SearchBarInHeaderSearch from "./pages/search-bar-in-header/search";
 import EcommerceHome from "./pages/ecommerce";
 import EcommerceSearch from "./pages/ecommerce/Search";
 import EcommerceCategory from "./pages/ecommerce/Category";
-import { ApmRoute } from "@elastic/apm-rum-react";
 import ListingPage from "./pages/ecommerce/ListingPage";
 
 export default function Router() {
   return (
     <div className="App">
       <Switch>
-        <ApmRoute exact path="/" component={Root} />
+        <Route exact path="/" component={Root} />
 
         {/* Connectors */}
-        <ApmRoute exact path="/elasticsearch" component={Elasticsearch} />
-        <ApmRoute exact path="/app-search" component={AppSearch} />
-        <ApmRoute exact path="/site-search" component={SiteSearch} />
-        <ApmRoute exact path="/workplace-search" component={WorkplaceSearch} />
-        <ApmRoute exact path="/engines" component={Engines} />
+        <Route exact path="/elasticsearch" component={Elasticsearch} />
+        <Route exact path="/app-search" component={AppSearch} />
+        <Route exact path="/site-search" component={SiteSearch} />
+        <Route exact path="/workplace-search" component={WorkplaceSearch} />
+        <Route exact path="/engines" component={Engines} />
 
         {/* Examples */}
-        <ApmRoute
+        <Route
           exact
           path="/search-as-you-type"
           component={SearchAsYouType}
         />
-        <ApmRoute
+        <Route
           exact
           path="/customizing-styles-and-html"
           component={CustomizingStylesAndHtml}
         />
-        <ApmRoute
+        <Route
           exact
           path="/search-bar-in-header"
           component={SearchBarInHeaderIndex}
         />
-        <ApmRoute
+        <Route
           exact
           path="/search-bar-in-header/search"
           component={SearchBarInHeaderSearch}
         />
-        <ApmRoute
+        <Route
           exact
           path="/elasticsearch-with-analytics"
           component={ElasticsearchWithAnalytics}
         />
 
         {/* Use cases */}
-        <ApmRoute exact path="/ecommerce/" component={EcommerceHome} />
-        <ApmRoute exact path="/ecommerce/search" component={EcommerceSearch} />
-        <ApmRoute exact path="/ecommerce/all" component={ListingPage} />
-        <ApmRoute
+        <Route exact path="/ecommerce/" component={EcommerceHome} />
+        <Route exact path="/ecommerce/search" component={EcommerceSearch} />
+        <Route exact path="/ecommerce/all" component={ListingPage} />
+        <Route
           exact
           path="/ecommerce/category/:category"
           component={EcommerceCategory}
