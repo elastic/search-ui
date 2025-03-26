c3652c046b13fa99c210196f794bc375a52aa305
saarikabhasi <nairsaarika.bhasi@elastic.co>
2025-03-26 10:35:58 -0400

    add actions as a type to searchDriver class

--- packages/search-ui/src/SearchDriver.ts
+++ packages/search-ui/src/SearchDriver.ts
@@ -24,6 +24,7 @@ import {
   RequestState,
   SortDirection
 } from "./types";
+import { SearchDriverActions } from "@elastic/search-ui";
 
 function filterSearchParameters({
   current,
@@ -145,7 +146,7 @@ interface SearchDriver extends actions.SearchDriverActions {
  */
 class SearchDriver {
   state: SearchState = DEFAULT_STATE;
-
+  actions: SearchDriverActions;
   debug: boolean;
   events: Events;
   plugins: Plugin[];
