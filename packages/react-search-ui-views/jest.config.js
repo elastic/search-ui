f40bea2a1a341f82806ff5d3886d152c7b29e73d
Yan Savitski <yan.savitski@elastic.co>
2025-05-22 18:30:21 +0200

    Install react-testing-library and update jest related packages

--- packages/react-search-ui-views/jest.config.js
+++ packages/react-search-ui-views/jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
+  testEnvironment: "jest-environment-jsdom",
   preset: "ts-jest/presets/js-with-ts-esm",
   testPathIgnorePatterns: ["./lib"],
-  testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
   moduleNameMapper: {
     "\\.(css)$": "<rootDir>/__mocks__/styleMock.js"
-  },
-  snapshotSerializers: ["enzyme-to-json/serializer"]
+  }
 };
--- packages/react-search-ui-views/src/setupTests.js
+++ packages/react-search-ui-views/src/setupTests.js
@@ -1,18 +1,2 @@
 // setup file
-import { configure } from "enzyme";
-import util from "util";
-import Adapter from "@cfaester/enzyme-adapter-react-18";
-
-configure({ adapter: new Adapter() });
-
-if (typeof global.TextEncoder === "undefined") {
-  Object.defineProperty(global, "TextEncoder", {
-    value: util.TextEncoder
-  });
-}
-
-if (typeof global.TextDecoder === "undefined") {
-  Object.defineProperty(global, "TextDecoder", {
-    value: util.TextDecoder
-  });
-}
+import "@testing-library/jest-dom";
